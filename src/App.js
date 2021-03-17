9b587a09a3bd5fadd40a12988f99e85b973f61af
SesH-iO <sesh.io1022@gmail.com>
2021-03-18 01:18:29 +0530

    Update our menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
+
+const HatsPage = () => {
+	return (
+		<div>
+			<h1>Hats Page</h1>
+		</div>
+	);
+};
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<HomePage />
+				<Switch>
+					<Route path='/shop/hats' component={HatsPage} />
+					<Route path='/' component={HomePage} />
+				</Switch>
 			</div>
 		);
 	}
